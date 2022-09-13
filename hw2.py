7fe365385edaa9301a794666c85a396a91c09ddf
larus_occidentalis <galieva.gen@gmail.com>
2022-09-13 18:08:39 +0300

    awrte

--- hw2.py
+++ hw2.py
@@ -1,20 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from typing import Any, List
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 #1. Создать список и заполнить его элементами различных типов данных. Реализовать скрипт проверки типа данных каждого элемента. Использовать функцию type() для проверки типа.
 # Элементы списка можно не запрашивать у пользователя, а указать явно, в программе.
 the_list = [4, 19.5, "genetic", None]
@@ -22,10 +5,10 @@ a = 4
 b = 19.5
 c = "genetic"
 d = None
-print (type(a))
-print (type(b))
-print (type(c))
-print (type(d))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
 
 
 #2. Для списка реализовать обмен значений соседних элементов. Значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т. д.
@@ -36,48 +19,52 @@ s[2] = 3
 s[3] = 2
 s[4] = 6
 s[5] = 5
-print (s)
+print(s)
+
+s = [1, 2, 3, 4, 5, 6]
+
+s[0], s[1] = s[1], s[0]
+s[2], s[3] = s[3], s[2]
+s[4], s[5] = s[5], s[4]
+print(s)
+
 
 
 # При нечётном количестве элементов последний сохранить на своём месте. Для заполнения списка элементов нужно использовать функцию input().
-#3. Пользователь вводит месяц в виде целого числа от 1 до 12. Сообщить, к какому времени года относится месяц (зима, весна, лето, осень). Напишите решения через list и dict.
+#3. Пользователь вводит месяц в виде целого числа от 1 до 12. Сообщить, к какому времени года относится месяц (зима, весна, лето, осень).
+# Напишите решения через list и dict.
 month = int(input("Введите номер месяца "))
 seas_list = ['winter', 'spring', 'summer', 'autumn']
-seas_dict = {0 : 'winter', 1 : 'spring', 2 : 'summer', 3 : 'autumn'}
+seas_dict = {0: 'winter', 1: 'spring', 2: 'summer', 3: 'autumn'}
 
 if month == 1 or month == 12 or month == 2:
-        print(seas_dict.get(0))
-        print(seas_list[0])
+     print(seas_dict.get(0))
+     print(seas_list[0])
 elif month == 3 or month == 4 or month == 5:
     print(seas_dict.get(1))
     print(seas_list[1])
 elif month == 6 or month == 7 or month == 8:
     print(seas_dict.get(2))
-    print(seasons_list[2])
+    print(seas_list[2])
 
 elif month == 9 or month == 10 or month == 11:
     print(seas_dict.get(3))
     print(seas_list[3])
 
-
-
-
-
-
-
-
-
 #4. Пользователь вводит строку из нескольких слов, разделённых пробелами. Вывести каждое слово с новой строки. Строки нужно пронумеровать.
 # Если слово длинное, выводить только первые 10 букв в слове.
 
-a_str = input("Введите данные ")
-a = str.split(' ')
-for i, el in enumerate(a, 1):
-    if len(el) > 10:
-        el = el [0:10]
-    print(f"{i}. - {el}")
+a_str = input("Введите данные: ").split()
+for i, val in enumerate(a_str):
+    if len(a_str) > 10:
+        print(i + 1, val[0:11])  #Здравствуйте! Я нашла метод enumerate и не могу понять, почему нельзя сделать срез для каждого значения,
+                                  # в данном случае для уже разделенной строки. Ошибку не выдает, но и не работает. Выбрала не тот метод?
+    else:
+        print(i + 1, val)
 
-#5. Реализовать структуру «Рейтинг», представляющую собой набор натуральных чисел, который не возрастает. У пользователя нужно запрашивать новый элемент рейтинга.
+
+#5. Реализовать структуру «Рейтинг», представляющую собой набор натуральных чисел, который не возрастает. У пользователя нужно запрашивать
+# новый элемент рейтинга.
 # Если в рейтинге существуют элементы с одинаковыми значениями, то новый элемент с тем же значением должен разместиться после них.
 #Подсказка. Например, набор натуральных чисел: 7, 5, 3, 3, 2.
 #Пользователь ввёл число 3. Результат: 7, 5, 3, 3, 3, 2.
@@ -100,7 +87,7 @@ for element in my_list:
         elif number < my_list[len(my_list) - 1]:
             my_list.append(number)
 
-
+print(my_list)
 #6. * Реализовать структуру данных «Товары». Она должна представлять собой список кортежей. Каждый кортеж хранит информацию об отдельном товаре.
 # В кортеже должно быть два элемента — номер товара и словарь с параметрами, то есть характеристиками товара: название, цена, количество, единица измерения.
 # Структуру нужно сформировать программно, запросив все данные у пользователя.
@@ -121,23 +108,10 @@ for element in my_list:
 #“ед”: [“шт.”]
 #}
 
-number = int (input ("Введите номер продукта: "))
+number = int(input("Введите номер продукта: "))
 prod = []
 features = {}
-value1 = input (str ("Название товара"))
-value2 = input (str ("Цена"))
-value3 = input (str ("Количество"))
-value4 = input (str("Единицы измерения"))
-
-
-features["Название товара"] = value1
-features["Цена"] = value2
-features["Количество"] = value3
-features["Единицы измерения"] = value4
-prod.append(tuple[number,features])
-print(prod)
-
-
-
-
-
+value1 = input(str("Название товара"))
+value2 = input(str("Цена"))
+value3 = input(str("Количество"))
+value4 = input(str("Единицы измерения"))
--- les33.py
+++ les33.py
@@ -1,7 +1,7 @@
 #1. Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
 #Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
-arg1 = float (input("Введите аргумент 1:"))
-arg2 = float (input("Введите аргумент 2:"))
+arg1 = float(input("Введите аргумент 1:"))
+arg2 = float(input("Введите аргумент 2:"))
 def my_divide (arg1, arg2):
     try:
         return arg1 / arg2
@@ -12,16 +12,16 @@ print(arg1 / arg2)
 #2. Выполнить функцию, которая принимает несколько параметров, описывающих данные пользователя:
 #имя, фамилия, год рождения, город проживания, email, телефон. Функция должна принимать параметры как именованные аргументы.
 #Осуществить вывод данных о пользователе одной строкой.
-name = str (input ("name"))
-surname = str (input ("surname"))
-date_of_birth = str (input("date_of_birth"))
-city = str (input("city"))
-email = str (input("email"))
-phone_number = str (input("phone_number"))
+name = str(input("name"))
+surname = str(input("surname"))
+date_of_birth = str(input("date_of_birth"))
+city = str(input("city"))
+email = str(input("email"))
+phone_number = str(input("phone_number"))
 
 def bios_data (name, surname, date_of_birth, city, email, phone_number):
     return bios_data.join([name, surname, date_of_birth, city, email, phone_number])
-print (name, surname, date_of_birth, city, email, phone_number)
+print(name, surname, date_of_birth, city, email, phone_number)
 
 #3. Реализовать функцию my_func(), которая принимает три позиционных аргумента и возвращает сумму наибольших двух аргументов.
 def my_func (arg1, arg2, arg3):
@@ -50,36 +50,48 @@ print (my_func)
 #Пользователь может продолжить ввод чисел, разделённых пробелом и снова нажать Enter. Сумма вновь введённых чисел будет добавляться к уже подсчитанной сумме.
 #Но если вместо числа вводится специальный символ, выполнение программы завершается. Если специальный символ введён после нескольких чисел,
 #то вначале нужно добавить сумму этих чисел к полученной ранее сумме и после этого завершить программу.
-def my_sum ():
-    sum_res = 0
-    ex = False
-    while ex == False:
-        number = input('Введите числа через пробел или e для завершения программы').split()
 
+def my_sum (*args):
+    sum_res = 0
+    try:
         res = 0
         for el in range(len(number)):
-            if number[el] == 'e':
-                ex = True
-                break
-            else:
-                res = res + int(number[el])
-        sum_res = sum_res + res
-        print(f'Current sum is {sum_res}')
-    print(f'Your final sum is {sum_res}')
+            res = res + int(number[el])
+            sum_res = sum_res + res
+            print(f'Current sum is {sum_res}')
+    except number[el] == '*':
+        return
+
+
+while 1:
+    i = input(">>>")#Запрос ввода
+    i = i.split(" ")#Делим по пробелам
+    a = 0
+    while a < len(i):
+        try:
+            i[a] = int(i[a])#строки в числа
+        except ValueError:
+            print("Value Error")#при ошибке пишем ошибку и останавливаем цикл
+            break
+        a = a + 1
+    z = 0#Переменная с результатом
+    for c in i:
+        z = z + c#Считаем все
+    print(z)#Выводим результат
 
 
 
 
 #6. Реализовать функцию int_func(), принимающую слова из маленьких латинских букв и возвращающую их же, но с прописной первой буквой.
 #Например, print(int_func(‘text’)) -> Text.
-i = str(input ("Введите текст с маленькой буквы: "))
+i = str(input("Введите текст с маленькой буквы: "))
 def int_func(*args):
-    return (i.title())
-print(i.title())
+    return i.title()
+    print(i)
 #7. Продолжить работу над заданием. В программуc должна попадать строка из слов, разделённых пробелом.
 #Каждое слово состоит из латинских букв в нижнем регистре. Нужно сделать вывод исходной строки,
 #но каждое слово должно начинаться с заглавной буквы. Используйте написанную ранее функцию int_func().
 i = str(input ("Введите текст с маленькой буквы: "))
 def int_func(*args):
-    return (i.title())
-print(i.title())
+    return i.capitalize()
+    print(i)
